bcdbb788d9d794b2963aad5b3e425c72bebd3df9
Fersantis <fe.santis@duocuc.cl>
2023-09-05 12:40:55 -0300

    commit 2

--- RegistrAPP/src/app/login/login.page.ts
+++ RegistrAPP/src/app/login/login.page.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   templateUrl: 'login.page.html',
   styleUrls: ['login.page.scss'],
+
+  imports: [MatButtonModule, MatDividerModule, MatIconModule],
+
+  
 })
 export class LoginPage {
   inputValue: string ='';
