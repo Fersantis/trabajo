cc7b2a2e0aa6db785088f6051d894a0f1f87e318
Fersantis <fe.santis@duocuc.cl>
2023-09-05 12:22:16 -0300

    commit 1

--- RegistrAPP/src/app/login/login.page.ts
+++ RegistrAPP/src/app/login/login.page.ts
@@ -8,6 +8,28 @@ import { NavController } from '@ionic/angular';
 })
 export class LoginPage {
   inputValue: string ='';
+  inputValue2: string ='';
+  errorMessage: string = '';
+
+  checkFieldLength() {
+    if (this.inputValue.length < 1) {
+      this.errorMessage = 'Debe ingresar al menos 1 dato';
+    } else {
+      this.errorMessage = '';
+    }
+  }
+
+  shouldDisableButton(): boolean {
+    return this.inputValue.length < 1 || this.inputValue2.length < 1;
+  }
+
+  checkFieldLength2() {
+    if (this.inputValue2.length < 1) {
+      this.errorMessage = 'Debe ingresar al menos 1 dato';
+    } else {
+      this.errorMessage = '';
+    }
+  }
 
   constructor(private navCtrl: NavController) {}
   Ingresar(){
