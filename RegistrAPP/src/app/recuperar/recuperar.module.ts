23f33358cd74e2fd991705890e6f579556d44e26
Fersantis <fe.santis@duocuc.cl>
2023-09-06 11:42:28 -0300

    commit 4

---
+++ RegistrAPP/src/app/recuperar/recuperar.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './recuperar.page';
+
+import { HomePageRoutingModule } from './recuperar-routing.module';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule
+  ],
+  declarations: [HomePage]
+})
+export class HomePageModule {}
---
+++ RegistrAPP/src/app/recuperar/recuperar.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-recuperar',
+  templateUrl: 'recuperar.page.html',
+  styleUrls: ['recuperar.page.scss'],
+})
+export class  RecuperarPage {
+
+    }
+
+
