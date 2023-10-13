b2b000660f0c2995317a345902eea466c7129544
Fersantis <fe.santis@duocuc.cl>
2023-10-13 14:34:11 -0300

    yuyju

--- RegistrAPP/src/app/asistencia.service.ts
+++ RegistrAPP/src/app/asistencia.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AsistenciaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
