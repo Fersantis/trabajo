370fa8dd0a275e9fa15b198324acf1cd78b78608
PANenduoc <129529411+PANenduoc@users.noreply.github.com>
2023-10-15 14:44:47 -0300

    :)

--- RegistrAPP/src/app/login/login.module.ts
+++ RegistrAPP/src/app/login/login.module.ts
@@ -21,7 +21,7 @@ import { HttpClientModule } from '@angular/common/http';
     MatButtonModule,
     MatDividerModule,
     MatIconModule,
-    HttpClientModule // Agrega HttpClientModule aquí
+    HttpClientModule
   ],
   declarations: [LoginPage]
 })
