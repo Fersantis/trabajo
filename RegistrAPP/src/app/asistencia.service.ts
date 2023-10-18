429b117c22906e58e7850867f211b21f66ddd0c1
Geraldine <geraldine.castrocc@gmail.com>
2023-10-18 12:15:06 -0300

    Update asistencia.service.ts

--- RegistrAPP/src/app/asistencia.service.ts
+++ RegistrAPP/src/app/asistencia.service.ts
@@ -12,7 +12,7 @@ export class AsistenciaService {
   getDatos(): Observable<any> {
     // Realiza la solicitud HTTP utilizando el HttpClient
     // Por ejemplo, aquí se realiza una solicitud GET a una URL determinada
-    const apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+    const apiUrl = 'http://localhost/phpmyadmin/index.php?route=/table/change&db=asistencia_duoc&table=profesor';
     return this.http.get<any>(apiUrl);
   }
 }
