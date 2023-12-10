195d62202f264d72d9506f61bab35f6c4e96ac7f
Geraldine <geraldine.castrocc@gmail.com>
2023-12-09 23:34:52 -0300

    api url cambio

--- RegistrAPP/src/app/login/api.service.ts
+++ RegistrAPP/src/app/login/api.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = 'https://stsxxdw5-5000.brs.devtunnels.ms/'; // Reemplaza con la URL de tu API
+  private apiUrl = 'https://stsxxdw5-5000.brs.devtunnels.ms/'; 
 
   constructor(private http: HttpClient) {}
 
